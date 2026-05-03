6964a2c4063eb7aa9cd9eb2ebc34cb6501ef9c8e
chenxz <220896428+chenxiaozhi81@users.noreply.github.com>
2026-05-03 20:35:05 +0800

    hellow2

--- factor.R
+++ factor.R
@@ -1,6 +1,15 @@
 # Hello World 函数
+#' 输出 "Hello World!" 消息
+#'
+#' 这是一个简单的函数，用于在控制台打印 "Hello World!" 消息。
+#'
+#' @return 无返回值，直接在控制台输出消息
+#' @export
+#' @examples
+#' hello_world()
 hello_world <- function() {
   print("Hello World!")
+  print("Hello World! 1")
 }
 
 # 调用函数
